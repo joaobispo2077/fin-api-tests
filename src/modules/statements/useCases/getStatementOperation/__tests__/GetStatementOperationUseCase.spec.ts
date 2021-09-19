e9c16e09486ff0b3a446dad2913e6f3422c39f12
João Bispo <joaobispo2077@gmail.com>
2021-09-19 18:22:19 -0300

    test: should be able to get statement by id and user id

---
+++ src/modules/statements/useCases/getStatementOperation/__tests__/GetStatementOperationUseCase.spec.ts
@@ -0,0 +1,51 @@
+
+
+import faker from 'faker';
+import { InMemoryUsersRepository } from '../../../../users/repositories/in-memory/InMemoryUsersRepository';
+import { OperationType } from '../../../entities/Statement';
+import { InMemoryStatementsRepository } from '../../../repositories/in-memory/InMemoryStatementsRepository';
+import { CreateStatementUseCase } from '../../createStatement/CreateStatementUseCase';
+import { ICreateStatementDTO } from '../../createStatement/ICreateStatementDTO';
+import { GetStatementOperationUseCase } from '../GetStatementOperationUseCase';
+
+const usersRepositoryInMemory = new InMemoryUsersRepository();
+const statementsRepositoryInMemory = new InMemoryStatementsRepository();
+
+const createStatementUseCase = new CreateStatementUseCase(
+  usersRepositoryInMemory,
+  statementsRepositoryInMemory
+);
+
+const sutGetStatementOperationUseCase = new GetStatementOperationUseCase(
+  usersRepositoryInMemory,
+  statementsRepositoryInMemory
+);
+
+describe('Use case - [GetStatementOperationUseCases]', () => {
+  it('should be able to get statement by id and user id', async () => {
+    const createdUser = await usersRepositoryInMemory.create({
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    });
+
+    const statement: ICreateStatementDTO = {
+      user_id: createdUser.id as string,
+      description: faker.lorem.words(),
+      amount: faker.datatype.number({
+        'min': 50,
+        'max': 250
+      }),
+      type: OperationType.DEPOSIT,
+    };
+
+    const createdStatement = await createStatementUseCase.execute(statement);
+
+    const recoveredStatement = await sutGetStatementOperationUseCase.execute({
+      statement_id: createdStatement.id as string,
+      user_id: createdUser.id as string
+    });
+
+    expect(recoveredStatement).toEqual(createdStatement);
+  });
+});
