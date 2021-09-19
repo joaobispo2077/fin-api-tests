4d6fd0fdf1311c36cfc7202696df3e4a3775e1f0
João Bispo <joaobispo2077@gmail.com>
2021-09-19 20:13:17 -0300

    chore: remove mock

--- tests/functional/users.test.ts
+++
@@ -1,7 +0,0 @@
-describe('Users functional tests', () => {
-  it('should create a user with status code 201', async () => {
-    const response = await global.testRequest.get('/api/v1/profile');
-    console.log(response.body);
-    expect(null).toBe(201);
-  });
-});
