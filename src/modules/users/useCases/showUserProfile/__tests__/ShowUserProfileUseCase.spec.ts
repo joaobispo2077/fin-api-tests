492ce2fb979a51d23eeb799b67a5abc17c4c2173
João Bispo <joaobispo2077@gmail.com>
2021-09-19 16:31:03 -0300

    test: should be able to show user profile by user id

---
+++ src/modules/users/useCases/showUserProfile/__tests__/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,28 @@
+import faker from 'faker';
+
+import { InMemoryUsersRepository } from "../../../repositories/in-memory/InMemoryUsersRepository";
+import { ShowUserProfileUseCase } from '../ShowUserProfileUseCase';
+
+const usersRepositoryInMemory = new InMemoryUsersRepository();
+const sutShowUserProfileUseCase = new ShowUserProfileUseCase(usersRepositoryInMemory);
+
+describe('Use case - [ShowUserProfileUseCase]', () => {
+  it('should be able to show user profile by user id', async () => {
+    const createdUser = await usersRepositoryInMemory.create({
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    });
+
+    const user = await sutShowUserProfileUseCase.execute(createdUser.id as string);
+
+    const expectedUser = {
+      id: expect.any(String),
+      name: expect.any(String),
+      email: expect.any(String),
+      password: expect.any(String),
+    };
+
+    expect(user).toEqual(expect.objectContaining(expectedUser));
+  });
+});
