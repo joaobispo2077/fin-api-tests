3f98d534b031416a28a7851eadd9bad05ba37762
João Bispo <joaobispo2077@gmail.com>
2021-09-19 16:03:58 -0300

    feat: setup app into functional tests

--- tests/functional/users.test.ts
+++ tests/functional/users.test.ts
@@ -1,5 +1,7 @@
 describe('Users functional tests', () => {
   it('should create a user with status code 201', async () => {
+    const response = await global.testRequest.get('/api/v1/profile');
+    console.log(response.body);
     expect(null).toBe(201);
   });
 });
--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,8 +1,12 @@
+import { app } from "../src/app";
+import supertest from 'supertest';
+
 beforeAll(async () => {
   console.log('setupTests');
+  global.testRequest = supertest(app);
+
 });
 
 afterAll(async () => {
   console.log('teardownTests');
-}
-);
+});
