fe6db235a5a3b39d1eec9a74c8f4901aad1a75d0
João Bispo <joaobispo2077@gmail.com>
2021-10-02 14:43:10 -0300

    feat: enrich balance with sender id

--- src/modules/statements/mappers/BalanceMap.ts
+++ src/modules/statements/mappers/BalanceMap.ts
@@ -1,28 +1,36 @@
 import { Statement } from "../entities/Statement";
 
 export class BalanceMap {
-  static toDTO({statement, balance}: { statement: Statement[], balance: number}) {
-    const parsedStatement = statement.map(({
-      id,
-      amount,
-      description,
-      type,
-      created_at,
-      updated_at
-    }) => (
-      {
+  static toDTO({
+    statement,
+    balance,
+  }: {
+    statement: Statement[];
+    balance: number;
+  }) {
+    const parsedStatement = statement.map(
+      ({
+        id,
+        amount,
+        description,
+        type,
+        created_at,
+        updated_at,
+        sender_id,
+      }) => ({
         id,
         amount: Number(amount),
         description,
         type,
         created_at,
-        updated_at
-      }
-    ));
+        updated_at,
+        sender_id,
+      })
+    );
 
     return {
       statement: parsedStatement,
-      balance: Number(balance)
-    }
+      balance: Number(balance),
+    };
   }
 }
