d1ce50c78d684a3fe8f54e54919819939e1cc152
João Bispo <joaobispo2077@gmail.com>
2021-10-31 14:16:11 -0300

    test: should be able to get balance when user is authenticated

--- tests/functional/statements.test.ts
+++ tests/functional/statements.test.ts
@@ -17,7 +17,10 @@ describe("Statements functional tests", () => {
     const { token } = response.body;
 
     const depositPayload = {
-      amount: faker.random.number({ min: 1, max: 1000 }),
+      amount: faker.datatype.number({
+        min: 10,
+        max: 1000,
+      }),
       description: faker.lorem.sentence(),
     };
 
@@ -37,6 +40,47 @@ describe("Statements functional tests", () => {
         description: expect.any(String),
         created_at: expect.any(String),
         updated_at: expect.any(String),
+      })
+    );
+  });
+
+  it("should be able to get balance when user is authenticated", async () => {
+    const user = {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    await global.testRequest.post("/api/v1/users").send(user);
+
+    const response = await global.testRequest
+      .post("/api/v1/sessions")
+      .send(user);
+
+    const { token } = response.body;
+
+    const depositPayload = {
+      amount: faker.datatype.number({
+        min: 10,
+        max: 1000,
+      }),
+      description: faker.lorem.sentence(),
+    };
+
+    await global.testRequest
+      .post("/api/v1/statements/deposit")
+      .set("Authorization", `Bearer ${token}`)
+      .send(depositPayload);
+
+    const balanceResponse = await global.testRequest
+      .get("/api/v1/statements/balance")
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(balanceResponse.status).toBe(200);
+    expect(balanceResponse.body).toEqual(
+      expect.objectContaining({
+        balance: expect.any(Number),
+        statement: expect.any(Array),
       })
     );
   });
