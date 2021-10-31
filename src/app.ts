277e3ede9e0129b4fc3108c11488c5d555a1bd6b
João Bispo <joaobispo2077@gmail.com>
2021-10-31 11:08:39 -0300

    refactor: rewrite app.ts to a class with database control and start/close control

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,96 @@
-import 'reflect-metadata';
-import 'express-async-errors';
+import "reflect-metadata";
+import "express-async-errors";
 
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import './database';
-import './shared/container';
-import { router } from './routes';
-import { AppError } from './shared/errors/AppError';
+import * as database from "./database";
+import "./shared/container";
+import { router } from "./routes";
+import { AppError } from "./shared/errors/AppError";
 
-const app = express();
+class App {
+  private app = express();
+  constructor(private port: number = 3333) {}
 
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/v1', router);
-
-app.use(
-  (err: Error, request: express.Request, response: express.Response, _next: express.NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message
-      });
-    }
-
-    return response.status(500).json({
-      status: "error",
-      message: `Internal server error - ${err.message} `,
-    });
+  public async init() {
+    await this.setupDatabase();
+    this.setupMiddlewares();
+    this.setupRoutes();
+    this.setupErrorHandler();
   }
-);
 
-export { app };
+  public start() {
+    this.app.listen(this.port, () =>
+      console.log(`Server is running at port ${this.port}`)
+    );
+  }
+
+  getApp() {
+    return this.app;
+  }
+
+  private async setupDatabase() {
+    await database.connect();
+  }
+
+  public async close() {
+    await database.close();
+  }
+
+  private setupMiddlewares() {
+    this.app.use(express.json());
+    this.app.use(cors());
+  }
+
+  private setupRoutes() {
+    this.app.use("/api/v1", router);
+
+    this.app.use(
+      (
+        err: Error,
+        request: express.Request,
+        response: express.Response,
+        next: express.NextFunction
+      ) => {
+        if (err instanceof AppError) {
+          return response.status(err.statusCode).json({
+            status: "error",
+            message: err.message,
+          });
+        }
+
+        console.error(err);
+
+        return response.status(500).json({
+          status: "error",
+          message: "Internal server error",
+        });
+      }
+    );
+  }
+
+  private setupErrorHandler() {
+    this.app.use(
+      (
+        err: Error,
+        request: express.Request,
+        response: express.Response,
+        _next: express.NextFunction
+      ) => {
+        if (err instanceof AppError) {
+          return response.status(err.statusCode).json({
+            message: err.message,
+          });
+        }
+
+        return response.status(500).json({
+          status: "error",
+          message: `Internal server error - ${err.message} `,
+        });
+      }
+    );
+  }
+}
+
+export { App };
