9183c770dddbb940bc47d91463ce50ec0e72453b
João Bispo <joaobispo2077@gmail.com>
2021-10-31 14:21:25 -0300

    test: should be able to withdraw when user is authenticated and has sufficient amount

--- tests/functional/statements.test.ts
+++ tests/functional/statements.test.ts
@@ -84,4 +84,59 @@ describe("Statements functional tests", () => {
       })
     );
   });
+
+  it("should be able to withdraw when user is authenticated and has sufficient amount", async () => {
+    const user = {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    await global.testRequest.post("/api/v1/users").send(user);
+
+    const response = await global.testRequest
+      .post("/api/v1/sessions")
+      .send(user);
+
+    const { token } = response.body;
+
+    const depositPayload = {
+      amount: faker.datatype.number({
+        min: 900,
+        max: 1000,
+      }),
+      description: faker.lorem.sentence(),
+    };
+
+    await global.testRequest
+      .post("/api/v1/statements/deposit")
+      .set("Authorization", `Bearer ${token}`)
+      .send(depositPayload);
+
+    const withdrawPayload = {
+      amount: faker.datatype.number({
+        min: 20,
+        max: 50,
+      }),
+      description: faker.lorem.sentence(),
+    };
+
+    const withdrawResponse = await global.testRequest
+      .post("/api/v1/statements/withdraw")
+      .set("Authorization", `Bearer ${token}`)
+      .send(withdrawPayload);
+
+    expect(withdrawResponse.status).toBe(201);
+    expect(withdrawResponse.body).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        user_id: expect.any(String),
+        type: "withdraw",
+        amount: expect.any(Number),
+        description: expect.any(String),
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      })
+    );
+  });
 });
