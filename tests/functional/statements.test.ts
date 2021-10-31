fdcd26d211c8c4972d5db4d75a0c61121a7b33da
João Bispo <joaobispo2077@gmail.com>
2021-10-31 14:41:03 -0300

    test: should be able to transfer amount to other users when user is authenticated

--- tests/functional/statements.test.ts
+++ tests/functional/statements.test.ts
@@ -185,4 +185,79 @@ describe("Statements functional tests", () => {
       })
     );
   });
+
+  it("should be able to transfer amount to other users when user is authenticated", async () => {
+    const user = {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    const otherUser = {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    await global.testRequest.post("/api/v1/users").send(user);
+    await global.testRequest.post("/api/v1/users").send(otherUser);
+
+    const responseAuthOtherUser = await global.testRequest
+      .post("/api/v1/sessions")
+      .send(otherUser);
+
+    const { token: tokenFromOtherUser } = responseAuthOtherUser.body;
+
+    const profileResponseFromOtherUser = await global.testRequest
+      .get("/api/v1/profile")
+      .set("Authorization", `Bearer ${tokenFromOtherUser}`);
+
+    const response = await global.testRequest
+      .post("/api/v1/sessions")
+      .send(user);
+
+    const { token } = response.body;
+
+    const depositPayload = {
+      amount: faker.datatype.number({
+        min: 10,
+        max: 1000,
+      }),
+      description: faker.lorem.sentence(),
+    };
+
+    await global.testRequest
+      .post("/api/v1/statements/deposit")
+      .set("Authorization", `Bearer ${token}`)
+      .send(depositPayload);
+
+    const transferPayload = {
+      amount: faker.datatype.number({
+        min: 10,
+        max: 40,
+      }),
+      description: faker.lorem.sentence(),
+    };
+
+    const statementResponse = await global.testRequest
+      .post(
+        `/api/v1/statements/transfer/${profileResponseFromOtherUser.body.id}`
+      )
+      .set("Authorization", `Bearer ${token}`)
+      .send(transferPayload);
+
+    expect(statementResponse.status).toBe(201);
+    expect(statementResponse.body).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        user_id: expect.any(String),
+        sender_id: expect.any(String),
+        type: "transfer",
+        amount: expect.any(Number),
+        description: expect.any(String),
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      })
+    );
+  });
 });
