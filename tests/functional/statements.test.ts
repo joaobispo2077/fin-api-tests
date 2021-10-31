e4c91bb07e0947baef3bf7ec221beefef411771b
João Bispo <joaobispo2077@gmail.com>
2021-10-31 14:29:56 -0300

    test: should be able to get statement detail when user is authenticated

--- tests/functional/statements.test.ts
+++ tests/functional/statements.test.ts
@@ -139,4 +139,50 @@ describe("Statements functional tests", () => {
       })
     );
   });
+
+  it("should be able to get statement detail when user is authenticated", async () => {
+    const user = {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    await global.testRequest.post("/api/v1/users").send(user);
+
+    const response = await global.testRequest
+      .post("/api/v1/sessions")
+      .send(user);
+
+    const { token } = response.body;
+
+    const depositPayload = {
+      amount: faker.datatype.number({
+        min: 10,
+        max: 1000,
+      }),
+      description: faker.lorem.sentence(),
+    };
+
+    const depositResponse = await global.testRequest
+      .post("/api/v1/statements/deposit")
+      .set("Authorization", `Bearer ${token}`)
+      .send(depositPayload);
+
+    const statementResponse = await global.testRequest
+      .get(`/api/v1/statements/${depositResponse.body.id}`)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(statementResponse.status).toBe(200);
+    expect(statementResponse.body).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        user_id: expect.any(String),
+        type: "deposit",
+        amount: expect.any(String),
+        description: expect.any(String),
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      })
+    );
+  });
 });
